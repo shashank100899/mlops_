570e4f4e8b967e8b1711e40fe8487304d46280d6
shashank100899 <107853506+shashank100899@users.noreply.github.com>
2022-11-07 20:08:08 +0530

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,6 +1,6 @@
 python --version
 pip install --upgrade azureml-sdk
-pip install azure-cli==2.20.0
+pip install azure-cli>=2.30.0
 pip install --upgrade azureml-sdk[cli]
 pip install pytest
 pip install pytest-cov
