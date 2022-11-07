c1c67e81f3ee375830a259e36a415dcf590a1d41
shashank <shashank1@TIGER03366.tigeranalytics.local>
2022-11-07 12:21:17 +0530

    updated

--- demo.py
+++ demo.py
@@ -1,18 +1,16 @@
 import os
-import pickle
+import dill
 from datetime import datetime
 
 import numpy as np
 import pandas as pd
-from flask import Flask
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-import mlflow
 
-now = datetime.now()
-time_stamp = now.strftime("%d:%m:%Y_%H:%M:%S")
+model_path = "output_folder"
+
 
 def metrics(predicted,actual):
     mae = mean_absolute_error(actual,predicted)
@@ -32,19 +30,14 @@ x_trained = standard_model.transform(x)
 
 x_train,x_test,y_train,y_test = train_test_split(x_trained,y) 
 
-experiment_name = "Mlflow_demo"
-mlflow.set_experiment(experiment_name)
+model = LinearRegression()
+model = model.fit(x_train,y_train)
+predicted = model.predict(x_test)
 
-with mlflow.start_run():
-    model = LinearRegression()
-    model = model.fit(x_train,y_train)
-    predicted = model.predict(x_test)
+mae , mse , r2 = metrics(y_test , predicted)
 
-    mae , mse , r2 = metrics(y_test , predicted)
+with open(os.path.join(model_path , "model.plk"),"wb") as file_object:
+    dill.dump(model,file_object)
 
-    mlflow.log_metric("mae" , mae)
-    mlflow.log_metric("mse" , mse)
-    mlflow.log_metric("r2"  ,r2)
 
-    mlflow.sklearn.log_model(model , "model")
 
